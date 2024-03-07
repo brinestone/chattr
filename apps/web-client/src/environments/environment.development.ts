092620593e6338e6ac99094d07a69a49f7edbfdc
Conrad Bekondo <philllittle302@gmail.com>
2024-03-07 18:44:52 +0100

    chore: update

--- apps/web-client/src/environments/environment.development.ts
+++ apps/web-client/src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyAr06Qh2fvuFp2hIdNFhwCOo5jOfpXPNnI",
+        authDomain: "chattr-8d770.firebaseapp.com",
+        databaseURL: "https://chattr-8d770-default-rtdb.firebaseio.com",
+        projectId: "chattr-8d770",
+        storageBucket: "chattr-8d770.appspot.com",
+        messagingSenderId: "414009952865",
+        appId: "1:414009952865:web:50b404a386369f81013820"
+      }
+};
