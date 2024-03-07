55b4bac32fb27532a75a77f895d2659d10562066
Conrad Bekondo <philllittle302@gmail.com>
2024-03-07 19:01:51 +0100

    chore: update 2

--- apps/web-client/src/app/app.component.ts
+++ apps/web-client/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   selector: 'chattr-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly primeNgConfig = inject(PrimeNGConfig);
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
+}
