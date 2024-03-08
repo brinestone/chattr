5d7902a7be296fb135b8b0a3f519c15f676f6a89
Conrad Bekondo <philllittle302@gmail.com>
2024-03-08 13:42:46 +0100

    feat: auth done

--- apps/web-client/src/app/services/user.service.ts
+++ apps/web-client/src/app/services/user.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, inject, signal, computed } from '@angular/core';
-import { Auth, signInWithPopup, GoogleAuthProvider, signOut } from '@angular/fire/auth';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Auth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut } from '@angular/fire/auth';
+import { FirebaseError } from 'firebase/app';
+import { catchError, from, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +14,21 @@ export class UserService {
     this.auth.onAuthStateChanged(this.principal.set);
   }
 
-  initSignIn() {
-    signInWithPopup(this.auth, new GoogleAuthProvider());
+  initEmailSignIn(email: string, password: string) {
+    return from(createUserWithEmailAndPassword(this.auth, email, password)).pipe(
+      catchError((error: Error) => {
+        if (error instanceof FirebaseError) {
+          if (error.message.includes('email-already-in-use')) {
+            return signInWithEmailAndPassword(this.auth, email, password);
+          }
+        }
+        return throwError(() => error);
+      })
+    )
+  }
+
+  initGoogleSignIn() {
+    return from(signInWithPopup(this.auth, new GoogleAuthProvider()));
   }
 
   signOut() {
